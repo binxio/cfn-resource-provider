c7cd5aa1997ffe0306ef9b3e7bf443038398d665
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2017-11-01 09:45:13 +0100

    bumped to version 0.8.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), 'r') as f:
     long_description = f.read()
 
-version = "0.8.2"
+version = "0.8.3"
 
 
 class PyTest(TestCommand):
